afcee8782674f1013b88e7c542a9c8c3f2086214
JamaykonCZ <jorge.mombach@gmail.com>
2022-11-16 15:39:52 -0300

    inicio projeto

---
+++ src/page/Index.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import Form from "../components/Form";
+import List from "@mui/material/List";
+import TodoItem from "../components/TodoItem";
+import {Container} from "@mui/material";
+
+export default function Index(){
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        setTodos([...todos, todo]);
+    }
+
+    const deleteTodo = (id) => {
+        var filtered = todos.filter((todo) => todo.id !== id);
+        setTodos(filtered);
+    }
+
+    return (
+        <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+            <Form addTodo={addTodo} />
+            <List sx={{  marginTop: "1em" }}>
+                {todos.map((todo) => (
+                    <div key={todo.id} style={{ marginTop: "1em" }}>
+                        <TodoItem todo={todo} deleteTodo={deleteTodo}/>
+                    </div>
+                ))}
+            </List>
+        </Container>
+    )
+}
